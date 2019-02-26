22c9b95a5249fa168310f2d911f53774fa18a869
OriNachum <ori.nachum@gmail.com>
2019-02-26 22:56:02 +0200

    Exposed LifxApi to basic Get requests

--- LifxLanController/LifxWebApi/Controllers/ApiController.cs
+++ LifxLanController/LifxWebApi/Controllers/ApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Lifx;
 using LifxCoreController;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -72,37 +73,85 @@ namespace LifxWebApi.Controllers
             return response.ToString() + ": " + message;
         }
 
-        /*
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        // GET api/values/5
+        [HttpGet("Refresh")]
+        public async Task<ActionResult<string>> GetRefreshBulbsAsync()
         {
-            return new string[] { "value1", "value2" };
+            (eLifxResponse response, string message) = await Lifx.RefreshBulbsAsync();
+
+            return response.ToString() + ": " + message;
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [HttpGet("Off/{label}")]
+        public async Task<ActionResult<string>> GetOffAsync(string label)
         {
-            return "value";
+            LightBulb lightBulb = Lifx.Lights.FirstOrDefault(x => x.Value.Label == label).Value;
+            await lightBulb.OffAsync();
+            return "";
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // GET api/values/5
+        [HttpGet("On/{label}")]
+        public async Task<ActionResult<string>> OnAsync(string label)
         {
+            LightBulb lightBulb = Lifx.Lights.FirstOrDefault(x => x.Value.Label == label).Value;
+            await lightBulb.OnAsync();
+            return "";
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // GET api/values/5
+        [HttpGet("SetBrightness/{label}/{brightness}")]
+        public async Task<ActionResult<string>> GetSetBrightnessAsync(string label, double brightness)
         {
+            LightBulb lightBulb = Lifx.Lights.FirstOrDefault(x => x.Value.Label == label).Value;
+            await lightBulb.SetBrightnessAsync(new Percentage(brightness));
+            return "";
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET api/values/5
+        [HttpGet("SetColor/{label}/{Hue}/{Saturation}")]
+        public async Task<ActionResult<string>> GetSetColorAsync(string label, int hue, double saturation)
         {
-        }*/
+            LightBulb lightBulb = Lifx.Lights.FirstOrDefault(x => x.Value.Label == label).Value;
+            await lightBulb.SetColorAsync(new Color(new Hue(hue), new Percentage(saturation)));
+            return "";
+        }
+
+
+        // GET api/values/5
+        [HttpGet("SetLabel/{label}/{newLabel}")]
+        public async Task<ActionResult<string>> GetSetLabelAsync(string label, string newLabel)
+        {
+            LightBulb lightBulb = Lifx.Lights.FirstOrDefault(x => x.Value.Label == label).Value;
+            await lightBulb.SetLabelAsync(new Label(newLabel));
+            return "";
+        }
+
+
+        // GET api/values/5
+        [HttpGet("SetPower/{label}/{onOffState}")]
+        public async Task<ActionResult<string>> GetSetPowerAsync(string label, string onOffState)
+        {
+            bool valid = !string.IsNullOrEmpty(onOffState);
+            valid = valid  && new string[] { "off", "on" }.Contains(onOffState.ToLower());
+            if (onOffState != null && valid)
+            {
+                Power power = onOffState.ToLower().Equals("on") ? Power.On : Power.Off;
+                LightBulb lightBulb = Lifx.Lights.FirstOrDefault(x => x.Value.Label == label).Value;
+                await lightBulb.SetPowerAsync(power);
+            }
+            return "";
+        }
+
+
+        // GET api/values/5
+        [HttpGet("SetTemperature/{label}/{temperature}")]
+        public async Task<ActionResult<string>> GetSetTemperatureAsync(string label, int temperature)
+        {
+            LightBulb lightBulb = Lifx.Lights.FirstOrDefault(x => x.Value.Label == label).Value;
+            await lightBulb.SetTemperatureAsync(new Temperature(temperature));
+            return "";
+        }
     }
 }
