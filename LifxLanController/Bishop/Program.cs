373f7b6e2e3d80c6c0a2329447f832466ffcc8c4
OriNachum <ori.nachum@gmail.com>
2019-04-15 01:40:16 +0300

    Added LifxStateController (For fetch and get state of application); Added Bishop - a runner to actively request scheduled commands

---
+++ LifxLanController/Bishop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bishop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
